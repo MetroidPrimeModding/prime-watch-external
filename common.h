96a1597081022ce46568b3c68641f4da995328ee
Pwootage <pwootage@gmail.com>
2020-06-09 22:25:30 -0600

    Minor fixes (no stdout in unix; need to use c in common.sh becuase of objective c

--- common.h
+++ common.h
@@ -1,8 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <cstdint>
-#include <cstddef>
+#include <stdint.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
--- unixdump.cpp
+++ unixdump.cpp
@@ -13,25 +13,25 @@ uint8_t* emuRAMAddressStart = 0;
 void findAndAttachProcess() {
   int pid = dolphinPid();
   if (pid < 0) {
-    cout << "Can't find dolphin! Launch dolphin first!" << endl;
+    cerr << "Can't find dolphin! Launch dolphin first!" << endl;
     exit(1);
   }
 
-  cout << "Dolphin found, PID " << pid << endl;
+  cerr << "Dolphin found, PID " << pid << endl;
   const string file_name = "/dolphin-emu." + to_string(pid);
   int fd = shm_open(file_name.c_str(), O_RDWR, 0600);
 
   if (fd < 0) {
-    cout << "Failed to open shared memory" << endl;
+    cerr << "Failed to open shared memory" << endl;
     exit(2);
   }
 
-  cout << "Opened shmem" << endl;
+  cerr << "Opened shmem" << endl;
 
   size_t size = 0x2040000;
   void *mem = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (mem == MAP_FAILED) {
-    cout << "failed to map shared memory" << endl;
+    cerr << "failed to map shared memory" << endl;
     close(fd);
     exit(3);
   }
